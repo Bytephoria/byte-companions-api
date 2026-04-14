372f2a9d760d94073e8e2a299de831a40138ae62
iAmForyyDev <iAmForyyDev@gmail.com>
2026-04-13 20:11:45 -0500

    feat(api): add CompanionPlayerLoadEvent and CompanionPlayerUnloadEvent

---
+++ src/main/java/team/bytephoria/bytecompanions/api/bukkit/event/CompanionPlayerLoadEvent.java
@@ -0,0 +1,41 @@
+package team.bytephoria.bytecompanions.api.bukkit.event;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+import team.bytephoria.bytecompanions.api.user.CompanionPlayer;
+
+public final class CompanionPlayerLoadEvent extends Event {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    private final Player player;
+    private final CompanionPlayer companionPlayer;
+
+    public CompanionPlayerLoadEvent(
+            final @NotNull Player player,
+            final @NotNull CompanionPlayer companionPlayer
+    ) {
+        this.player = player;
+        this.companionPlayer = companionPlayer;
+    }
+
+    public Player player() {
+        return this.player;
+    }
+
+    public CompanionPlayer companionPlayer() {
+        return this.companionPlayer;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+}
