624f65655a87361b9e0c7777dd27b46a9a33ddfc
iAmForyyDev <iAmForyyDev@gmail.com>
2026-04-15 15:45:46 -0500

    fix(api): correct annotations dependency and add Maven Central repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ publishing {
 }
 
 repositories {
+    mavenCentral()
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
@@ -20,7 +21,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrain:annotations:26.1.0")
+    compileOnly("org.jetbrains:annotations:26.1.0")
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
