278da3e132bda3b49f71be17fd7fee30b2c8df37
iAmForyyDev <iAmForyyDev@gmail.com>
2026-04-15 15:41:49 -0500

    build(api): add PaperMC repository and compileOnly dependencies (paper-api, annotations)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,17 @@ publishing {
         }
     }
 }
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrain:annotations:26.1.0")
+}
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
